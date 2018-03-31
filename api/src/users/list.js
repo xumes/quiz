5e2124f10f0c59eb163463bc2505cb3908f998a7
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-03-31 00:35:33 -0300

    fixed issue on backend

--- api/src/users/list.js
+++ api/src/users/list.js
@@ -1,10 +1,10 @@
 import User from './../models/User'
 
 export default async (req, res) => {
-    let user = []
+    let users = []
     try{
-        user = await User.find({})
-        return res.json({user})
+        users = await User.find({})
+        return res.json({users})
     } catch(err){
         return res.status(500).json({err})
     }
