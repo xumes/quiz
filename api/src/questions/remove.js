1b2d80dc796e95c7521117dff1a8676ddde102ab
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-04-01 23:18:18 -0300

    fixing issue when remove a Question (cascading to the Subject list)

--- api/src/questions/remove.js
+++ api/src/questions/remove.js
@@ -1,8 +1,16 @@
 import Question from './../models/Question'
+import Subject from './../models/Subject'
 
 export default async (req, res) => {
     try{
-        await Question.findByIdAndRemove(req.params.id)
+        let question = await Question.findById(req.params.id).populate('subject')
+        let subject = await Subject.findById(question.subject._id)
+        subject.questions.pull({_id: question._id})
+
+        await subject.save()
+        await question.remove()
+
+        //await Question.findByIdAndRemove(req.params.id)
         return res.status(202).end()
     } catch(err){
         return res.status(500).end()
